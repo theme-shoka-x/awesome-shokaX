a593f63c1460ff8a3b5eae6bcb958c882ca5646c
zkz098 <zkz20081204@126.com>
2023-01-18 16:29:04 +0800

    docs: readme

--- extensions/qweather/qweather.js
+++ extensions/qweather/qweather.js
@@ -1,5 +1,5 @@
 hexo.extend.filter.register('theme_inject', function(injects) {
-    injects.footer.file('qweather', 'views/qweather.pug', {}, {cache: true});
+    injects.rightNav.file('qweather', 'views/qweather.pug', {}, {cache: true});
     injects.bodyEnd.file('loader','views/loader.pug', {}, {cache: true})
     injects.style.push('views/qweather.styl');
 });
